bd10681d9f7914bb5a9a33b095517f4d95a1a6ac
sosin <svstar94@naver.com>
2022-12-22 12:13:36 +0900

    add okr page

---
+++ src/styles/okr.ts
@@ -0,0 +1,101 @@
+import styled from "styled-components";
+
+export const OkrContainer = styled.div`
+  overflow-y: auto;
+`;
+
+export const OkrBox = styled.div`
+  padding: 0.5rem 1rem;
+  min-height: 120px;
+  background-color: #f6f6f6;
+  border-radius: 20px;
+  margin-bottom: 1rem;
+`;
+
+export const OkrHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  gap: 1rem;
+  height: 30px;
+  align-items: center;
+`;
+
+export const OkrCategory = styled.div`
+  flex: 4;
+  background-color: #d9d9d9;
+  border-radius: 10px;
+  height: 100%;
+  display: flex;
+  padding-left: 1rem;
+  justify-content: left;
+  align-items: center;
+  font-weight: 700;
+`;
+
+export const OkrDDay = styled.span`
+  flex: 1;
+  text-align: right;
+`;
+
+export const OkrTitle = styled.div`
+  margin-top: 0.5rem;
+  font-weight: 700;
+`;
+
+export const OkrKrs = styled.div``;
+
+export const KrsBox = styled.div`
+  margin-top: 0.5rem;
+  display: flex;
+  justify-content: flex-start;
+  gap: 1rem;
+`;
+
+export const KrsCircle = styled.div`
+  border-radius: 50px;
+  width: 16px;
+  height: 16px;
+  background-color: #d9d9d9;
+`;
+
+export const KrsTitle = styled.span``;
+
+export const OkrFooter = styled.div`
+  margin-top: 0.5rem;
+  display: flex;
+  gap: 1rem;
+`;
+
+export const OkrStatusBox = styled.div`
+  flex: auto;
+  border-radius: 10px;
+  background-color: #d9d9d9;
+`;
+export const OkrStatusFill = styled.div`
+  border-radius: 10px;
+`;
+
+export const OkrDetailBtn = styled.div``;
+
+export const OktBtnBox = styled.div`
+  position: fixed;
+  margin: 0 auto;
+  left: 0;
+  right: 0;
+  bottom: 60px;
+`;
+
+export const OktAddBtn = styled.button`
+  margin: 1rem auto;
+  width: 260px;
+  height: 40px;
+  border-radius: 30px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  border: 0;
+  background-color: #d9d9d9;
+  &:hover {
+    background-color: #8f8f8f;
+  }
+`;
