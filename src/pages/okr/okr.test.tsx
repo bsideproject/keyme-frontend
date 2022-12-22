a750e394166dd016835d2be460427930acf25dd8
sosin <svstar94@naver.com>
2022-12-22 12:13:36 +0900

    add okr page

--- src/pages/okr/okr.test.tsx
+++ src/pages/okr/okr.test.tsx
@@ -9,17 +9,8 @@ describe("OKR 화면 테스트", () => {
     // 로고 체크
     screen.getByRole("logo");
 
-    // 환영문구 체크
-    screen.getByText("반갑습니다. /[가-힣0-9a-zA-Z]{2:8}/님!");
-
-    // 검색버튼 체크
-    screen.getByAltText("search");
-
-    // 알람버튼 체크
-    screen.getByAltText("alarm");
-
-    // 마이페이지 프로필 체크
-    screen.getByAltText("profile");
+    // 누구누구의 OKR
+    screen.getByText(/[a-zA-Z가-힣]+님의 OKR/i);
   }),
     it("홈 화면 OKR 데이터 통신 체크", () => {
       render(<OKR />);
@@ -29,35 +20,39 @@ describe("OKR 화면 테스트", () => {
 
       // Key Result 체크
       screen.findByText("Key Result");
-
-      // Initialtive 체크
-      screen.findByText("Initiative");
     }),
-    it("홈 화면 OKR 등록 모달 체크", () => {
+    it("홈 화면 OKR 상세 페이지 체크", () => {
       render(<OKR />);
 
-      // OKR 추가 버튼 클릭
-      const btn = screen.getByAltText("addokr");
-      fireEvent.click(btn);
-
-      // OKR 등록 모달 오픈 체크 (제목)
-      screen.getByText("OKR 추가");
-
-      // OKR 카테고리 확인 (목표, KR, 투두)
-      screen.getByText("목표");
-
-      // OKR 등록 form 체크
-      screen.getByRole("form");
-
-      // OKR 등록 버튼 클릭
-      const submitBtn = screen.getByAltText("submitokr");
-      fireEvent.click(submitBtn);
-
-      // OKR 등록 form 양식 체크
-
-      // 등록 후 모달 close 체크
-      expect(screen.queryByText("OKR 추가")).toHaveLength(0);
-
-      // 데이터 등록 후 홈 화면 OKR 데이터 체크
+      // 더보기 버튼 클릭
     });
+  it("홈 화면 OKR 등록 모달 체크", () => {
+    render(<OKR />);
+
+    // OKR 추가 버튼 클릭
+    const btn = screen.getByText("+ OKR 만들기");
+    fireEvent.click(btn);
+
+    // // OKR 등록 모달 오픈 체크 (제목)
+    // screen.getByText("OKR 만들기");
+    // // 설명
+    // screen.getByText("어떤 목표를 가지고 계신가요?");
+
+    // // OKR 카테고리
+    // screen.queryAllByRole("category");
+
+    // // OKR 등록 form 체크
+    // screen.getByRole("form");
+
+    // // OKR 등록 버튼 클릭
+    // const submitBtn = screen.getByAltText("submitokr");
+    // fireEvent.click(submitBtn);
+
+    // // OKR 등록 form 양식 체크
+
+    // // 등록 후 모달 close 체크
+    // expect(screen.queryByText("OKR 추가")).toHaveLength(0);
+
+    // // 데이터 등록 후 홈 화면 OKR 데이터 체크
+  });
 });
--- src/pages/okr/okr.tsx
+++ src/pages/okr/okr.tsx
@@ -1,15 +1,147 @@
 import React from "react";
 
-import { useUser } from "@hooks/useUser";
+// import { useUser } from "@hooks/useUser";
+import {
+  KrsBox,
+  KrsCircle,
+  KrsTitle,
+  OkrBox,
+  OkrCategory,
+  OkrContainer,
+  OkrDDay,
+  OkrDetailBtn,
+  OkrFooter,
+  OkrHeader,
+  OkrKrs,
+  OkrStatusBox,
+  OkrStatusFill,
+  OkrTitle,
+  OktAddBtn,
+  OktBtnBox,
+} from "@styles/okr";
+
+import "./okr.css";
 
 function Okr() {
   // const { user } = useUser();
 
+  const okrList = [
+    {
+      id: 1,
+      category: "업무",
+      categoryColor: 1,
+      title: "Objective",
+      dDay: 90,
+      krList: [
+        {
+          id: 1,
+          title: "key result 1",
+          isComplete: false,
+        },
+        {
+          id: 2,
+          title: "key result 2",
+          isComplete: false,
+        },
+      ],
+      progress: "64%",
+    },
+    {
+      id: 2,
+      category: "건강",
+      categoryColor: 2,
+      title: "안전한 테스트 익히기",
+      dDay: 0,
+      krList: [
+        {
+          id: 3,
+          title: "테스트 커버리지 90% 달성",
+          isComplete: false,
+        },
+        {
+          id: 4,
+          title: "Spring boot 통합 테스트 병렬실행",
+          isComplete: false,
+        },
+      ],
+      progress: "38%",
+    },
+    {
+      id: 3,
+      category: "기타",
+      categoryColor: 3,
+      title: "안전한 테스트 익히기",
+      dDay: 365,
+      krList: [
+        {
+          id: 5,
+          title: "테스트 커버리지 90% 달성",
+          isComplete: false,
+        },
+        {
+          id: 6,
+          title: "Spring boot 통합 테스트 병렬실행",
+          isComplete: false,
+        },
+      ],
+      progress: "92%",
+    },
+  ];
+
+  const categoryColorPicker = ["#d9d9d9", "#f80000", "#ff9900"];
+
   return (
-    <div>
-      {/*<div> name: {user?.name}</div>*/}
-      {/*<div> email: {user?.email}</div>*/}
-      <div>Okr</div>
+    <div id="okr">
+      <div className="logo" role={"logo"}>
+        로고
+      </div>
+      {/* <div>{user?.name}님의 OKR</div> */}
+      <div>짐니님의 OKR</div>
+
+      {/* OKR 컴포넌트 (목록식) */}
+      <OkrContainer>
+        {okrList.map(({ id, category, dDay, categoryColor, title, krList, progress }) => {
+          return (
+            <OkrBox key={id}>
+              <OkrHeader>
+                <OkrCategory style={{ backgroundColor: categoryColorPicker[categoryColor] }}>
+                  {category}
+                </OkrCategory>
+                <OkrDDay>{dDay ? `D-${dDay}` : ""}</OkrDDay>
+              </OkrHeader>
+
+              <OkrTitle>{title}</OkrTitle>
+              <OkrKrs>
+                {/* key results 목록 */}
+                {krList
+                  ? krList.map(({ id, title, isComplete }) => {
+                      return (
+                        <KrsBox key={id}>
+                          <KrsCircle style={{ backgroundColor: isComplete ? "#ff9494" : "" }}>
+                            &nbsp;
+                          </KrsCircle>
+                          <KrsTitle>{title}</KrsTitle>
+                        </KrsBox>
+                      );
+                    })
+                  : ""}
+              </OkrKrs>
+              <OkrFooter>
+                <OkrStatusBox>
+                  <OkrStatusFill
+                    style={{ backgroundColor: "#FF9494", width: progress }}></OkrStatusFill>
+                </OkrStatusBox>
+                <OkrDetailBtn>더보기</OkrDetailBtn>
+              </OkrFooter>
+            </OkrBox>
+          );
+        })}
+      </OkrContainer>
+      <OktBtnBox>
+        <OktAddBtn>+ OKR 만들기</OktAddBtn>
+      </OktBtnBox>
+
+      {/* 생성 모달, Detail 페이지 이동? 그러면 애니메이션 넣을 수 없지않나 위에서 아래로, 현재 위치에서 펼쳐지는 느낌으로 해야함 */}
     </div>
   );
 }
